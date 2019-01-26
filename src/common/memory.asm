ee6c85240ac9dd5f2e5484f0fbd440ae73aaf9c0
Emily Amanda Bellows <emily.a.bellows@gmail.com>
2019-01-26 01:13:02 -0500

    wrote some more memory.asm for fun, PLUS unit tests

--- src/common/memory.asm
+++ src/common/memory.asm
@@ -1,26 +1,40 @@
-; Does using this kind of PUSHS format make sense?
-
-PUSHS
-SECTION "memory.asm_rom0",ROM0
-
-; TODO: Start using BC as counter
-; 	dec bc
-;	  ld a, b
-;	  or c
-;	  jr nz, .ByteFill
-
-
 ; HL = Destination
 ; B = Number of bytes to write
 ; A = The byte to write
 memset:
-  ; TODO: Check for b = 0?
-.loop:
   ld [hl+], a
   dec b
-  jr nz, .loop
-.exit:
+  jr nz, memset
   ret
+
+; HL = Destination
+; BC = Number of bytes to write
+; A = The byte to write
+; clobbers D
+memset_bc:
+  ld d, a
+.loop:
+  ld a, d
+  ld [hl+], a
+  dec bc
+  ld a, b
+  or c
+  jp nz, .loop
+  ret
+
+
+; HL = Destination
+; BC = Number of zero bytes to write
+; clobbers A
+memzero:
+  xor a
+  ld [hl+], a
+  dec bc
+  ld a, b
+  or c
+  jr nz, memzero
+  ret
+
 
 ; HL = Destination
 ; B = Number of 16 bytes blocks to write
@@ -40,30 +54,35 @@ ENDR
 ; DE = Source
 ; B = Number of bytes to copy
 memcpy:
-  ; TODO: Check for b = 0?
-.loop:
   ld a, [de]
   inc de
   ld [hl+], a
   dec b
-  jr nz, .loop
-.exit:
+  jr nz, memcpy
+  ret
+
+; HL = Destination
+; DE = Source
+; BC = Number of bytes to copy
+memcpy_bc:
+  ld a, [de]
+  inc de
+  ld [hl+], a
+  dec bc
+  ld a, b
+  or c
+  jr nz, memcpy_bc
   ret
 
 ; HL = Destination
 ; DE = Source
 ; B = Number of 16 byte blocks top copy
 memcpy_16:
-  ; TODO: Check for b = 0?
-.loop:
 REPT 16
   ld a, [de]
   inc de
   ld [hl+], a
 ENDR
   dec b
-  jr nz, .loop
-.exit:
+  jr nz, memcpy_16
   ret
-
-POPS
--- src/projects/testing/main.asm
+++ src/projects/testing/main.asm
@@ -1,9 +1,13 @@
 INCLUDE "test_framework.asm"
+INCLUDE "memory.asm"
 
 test_main:
   call test_or_behaviour
   call test_cp_behaviour
   call test_dec16_behaviour
+  call test_memset_bc
+  call test_memzero
+  call test_memcpy_bc
   ret
 
 test_or_behaviour:
@@ -41,3 +45,87 @@ test_dec16_behaviour:
   dec bc
   test_assert_false z
   ret
+
+test_memset_bc:
+  test_plan 1
+
+  ld hl, memset_test_ram
+  ld bc, memset_test_ram.end - memset_test_ram
+  ld a, 3
+  call memset_bc
+
+  ld hl, memset_test_ram
+  ld bc, memset_test_ram.end - memset_test_ram
+  call sum_byte_range
+
+  cp 3*5
+  test_assert_true z
+  ret
+
+test_memzero:
+  test_plan 1
+
+  ld hl, memzero_test_ram
+  ld bc, memzero_test_ram.end - memzero_test_ram
+  call memzero
+
+  ld hl, memzero_test_ram
+  ld bc, memzero_test_ram.end - memzero_test_ram
+  call sum_byte_range
+
+  or a
+  test_assert_true z
+  ret
+
+test_memcpy_bc:
+  test_plan 1
+
+  ld hl, memcpy_bc_test_ram
+  ld de, memcpy_bc_test_data
+  ld bc, memcpy_bc_test_data.end - memcpy_bc_test_data
+  call memcpy_bc
+
+  ld hl, memcpy_bc_test_ram
+  ld bc, memcpy_bc_test_data.end - memcpy_bc_test_data
+  call sum_byte_range
+
+  cp a, 1+2+3+4+5+100
+  test_assert_true z
+  ret
+
+
+; HL = source
+; BC = number of bytes
+; outputs total in A
+; clobbers d
+sum_byte_range:
+  xor a
+  ld d, a
+.loop:
+  ld a, d
+  add a, [hl]
+  ld d, a
+  inc hl
+  dec bc
+  ld a, b
+  or c
+  jp nz, .loop
+  ld a, d
+  ret
+
+SECTION "Test ROM", ROM0
+memcpy_bc_test_data:
+DB 1,2,3,4,5,100
+.end:
+
+SECTION "Test RAM", WRAM0
+memset_test_ram:
+DS 5
+.end:
+
+memzero_test_ram:
+DS $200
+.end:
+
+memcpy_bc_test_ram:
+DS (memcpy_bc_test_data.end - memcpy_bc_test_data)
